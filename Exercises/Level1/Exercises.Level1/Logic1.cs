6580c0b22b2979c1b71f976e61f55278806d0765
korgz <Oskars.Kubulis@gmail.com>
2022-05-02 20:36:38 +0300

    ch3

--- Exercises/Level1/Exercises.Level1/Logic1.cs
+++ Exercises/Level1/Exercises.Level1/Logic1.cs
@@ -256,6 +256,7 @@ public class Logic1
         return false;
     }
 
+
     /// <summary>
     /// Given a number n, return true if n is in the range 1..10, inclusive. Unless outsideMode is
     /// true, in which case return true if the number is less or equal to 1, or greater or equal to
